ad48faf18408b41617c0c722b9d02e7f51913986
TASTAS <tmaallem@gmail.com>
2023-11-07 13:25:15 +0100

    minor changes

--- src/main/java/com/example/run/ProduitServlet.java
+++ src/main/java/com/example/run/ProduitServlet.java
@@ -14,17 +14,12 @@ public class ProduitServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // Récupérez l'ID du produit depuis la requête (par exemple, à partir de l'URL)
         String productId = request.getParameter("id");
 
-        // Utilisez un contrôleur de base de données pour récupérer les détails du produit spécifié
         DataBaseController controller = new DataBaseController();
         Produit produit = controller.getProductById(Integer.parseInt(productId));
 
-        // Ajoutez le produit à la requête pour qu'il puisse être affiché dans produit.jsp
         request.setAttribute("produit", produit);
-
-        // Dirigez vers la page produit.jsp
         request.getRequestDispatcher("/produit.jsp").forward(request, response);
 
         List<VarianteProduit> variantes = controller.getVariantesByProductId(Integer.parseInt(productId));
